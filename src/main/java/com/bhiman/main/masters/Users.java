1d67b9a273cb04b49f11e19660fc5e9831cc966d
mingalkar <mingalkar@gmail.com>
2021-02-23 17:19:57 +0530

    update for users

--- src/main/java/com/bhiman/main/masters/Users.java
+++ src/main/java/com/bhiman/main/masters/Users.java
@@ -1,5 +1,44 @@
 package com.bhiman.main.masters;
 
-public class Users {
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.bhiman.main.UIKeywords;
+
+public class Users extends UIKeywords {
+	private static final Logger LOG = Logger.getLogger(Banks.class);
+	
+	// Page Object Locators for Users page in Masters --> ******************Initialize at runtime*****************
+	
+	@FindBy(xpath = "//i[@class='fa fa-database']")			 
+	private static WebElement masters;	
+	
+	@FindBy(xpath = "//a[normalize-space(text())='Users']")			 
+	private static WebElement users;	
+	
+	@FindBy(css = "#add_btn")			 
+	private static WebElement add_users;
+	
+	@FindBy()
+	private static WebElement action_view_icon;
+	
+	@FindBy()
+	private static WebElement action_edit_icon;
+
+	@FindBy()
+	private static WebElement action_delete_icon;
+	
+	// Page Object Locators after click on AddUsers button in Users page
+	@FindBy(css ="user_name")
+	private static WebElement U_name;
+	
+	@FindBy(css ="#mobile_no")
+	private static WebElement U_mobile_no;
+	
+	@FindBy(css ="#email")
+	private static WebElement U_email;
+	
+	
 
 }
