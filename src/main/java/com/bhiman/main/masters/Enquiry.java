173004841f7075a0aca784d6dd5ccb63fd6b79f3
fareen <fareens1028@gmail.com>
2021-02-25 18:35:41 +0530

    Second commit

--- src/main/java/com/bhiman/main/masters/Enquiry.java
+++ src/main/java/com/bhiman/main/masters/Enquiry.java
@@ -14,7 +14,8 @@ public class Enquiry extends UIKeywords{
 	public Enquiry(){
 		PageFactory.initElements(Constants.driver, this);
 	}
-	//Enquiry page
+	
+	
 	//  Locators for Enquire page in Masters 
 		
 		@FindBy(xpath ="//span[text()='Masters']")			 
@@ -23,7 +24,7 @@ public class Enquiry extends UIKeywords{
 		@FindBy(xpath ="//a[text()='Enquiry']")
 		private static WebElement enquiry;
 		
-		   //Locator for  After click on Add Button in Enquire page.
+		   //Locator for  After click on Add Button in Enquire valid page .
 		@FindBy(id="add_btn")
 		private static WebElement addButton;
 		
@@ -42,13 +43,66 @@ public class Enquiry extends UIKeywords{
 		@FindBy(name="enquiry_add")
 		private static WebElement submitEnquiryButton;
 		
+		@FindBy(name="//input[@value='Cancel']")
+		private static WebElement cancelEnquiryButton;
 		
-		//Method for Enquire page in Masters
+		@FindBy(xpath="//*[text()='OK']")
+		private static WebElement okButton;
+		
+		@FindBy(xpath="(//*[@id='datatable3']//td[2])[1]")
+		private static WebElement savedName;
+		
+		@FindBy(xpath ="(//*[@id='datatable3']//td[3])[1]")
+		private static WebElement savedMobileNumber;
+		
+		@FindBy(xpath ="(//*[@id='datatable3']//td[4])[1]")
+		private static WebElement savedPurpose;
+		
+		@FindBy(xpath="(//*[@id='datatable3']//td[5])[1]")
+		private static WebElement savedReference;
+		
+//-------------------invalid Mobile number locator-------------------------------------------------------------
+		
+		@FindBy(id="name")
+		private static WebElement invalidName;
+		
+		@FindBy(id="mobile_no-error")
+		private static WebElement invalidMobileNumber;
+		
+		
+//--------------------------------------------------------------------------------------		
+		//Locator for enquire homePage on button(Copy,Excel,Csv,Pdf,Print)
+		@FindBy(xpath = "//span[text()='Copy']")
+		private static WebElement copyButton;
+
+		@FindBy(xpath = "//span[text()='Excel']")
+		private static WebElement excelButton;
+
+		@FindBy(xpath = "//span[text()='CSV']")
+		private static WebElement csvButton;
+
+		@FindBy(xpath = "//span[text()='PDF']")
+		private static WebElement pdfButton;
+
+		@FindBy(xpath = "//span[text()='Print']")
+		private static WebElement printButton;
+		
+		//Action button
+		
+		@FindBy(xpath = "//button[@data-id='18']")
+		private static WebElement reNamebutton;
+		
+//----------------Search button-------------------------------------------------------------------
+		@FindBy(xpath = "//input[@type='search']")
+		private static WebElement searchButton;
+		
+//---------------Method for Enquire page in Masters----------------------------
 		public void goToEnquiryPage(){
 			mouseHover(masters);
 			clickOnElement(enquiry);
 		}
 		
+		//When click on addButton Method
 		public void clickOnAddEnquiry(){
 			clickOnElement(addButton);
 		}
@@ -72,6 +126,51 @@ public class Enquiry extends UIKeywords{
 		public void submit(){
 			clickOnElement(submitEnquiryButton);
 		}
+		
+		public String getSavedName(){
+			return getText(savedName);
+		}
+		
+		public String getSavedMobileNumber(){
+			return getText(savedMobileNumber);
+		}
+		public String getSavedPurpose(){
+			return getText(savedPurpose);
+		}
+		public String getSavedReference(){
+			return getText(savedReference);
+		}
+		public void clickOnOkButton(){
+			clickOnElement(okButton);
+		}
+		
+		public void clickOnCancelButton(){
+			clickOnElement(okButton);
+		}
+		
+		//Method for button(copy,excel,csv,pdf,print)
+		public Boolean isCopyButtonDisplayed(){
+			return copyButton.isDisplayed();
+		}
+		
+		public Boolean isExcelButtonDisplayed(){
+			return excelButton.isDisplayed();
+		}
+		
+		public Boolean isCsvButtonDisplayed(){
+			return csvButton.isDisplayed();
+		}
+		
+		public Boolean isPdfButtonDisplayed(){
+			return pdfButton.isDisplayed();
+		}
+		
+		public Boolean isPrintButtonDisplayed(){
+			return printButton.isDisplayed();
+		}
+		
+		
+		//#mobile_no-error
 		
 
 }
