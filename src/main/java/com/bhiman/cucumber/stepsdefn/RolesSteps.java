e6bbb1c46b779af63ee3c8459cafcbe2dae56add
Shweta-Mahadik <mahadikshweta@gmail.com>
2021-03-09 10:19:39 +0530

    Updates Roles feature

--- src/main/java/com/bhiman/cucumber/stepsdefn/RolesSteps.java
+++ src/main/java/com/bhiman/cucumber/stepsdefn/RolesSteps.java
@@ -1,5 +1,15 @@
 package com.bhiman.cucumber.stepsdefn;
 
-public class RolesSteps {
 
+public class RolesSteps {
+	
+	public static void main(String[] args) {
+		
+		String s1 ="Java Selenium";
+		String s2 = "Java"+"Selenium";
+		System.out.println(s1==s2);
+		
+		
+	}
+ 
 }
